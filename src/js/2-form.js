a47b054e9248cdb948a0592087995c2cc8ff0a17
AlyonaBasko <33734752+AlyonaBasko@users.noreply.github.com>
2024-03-29 21:00:34 +0100

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,40 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector(".feedback-form");
-    const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
-    if (savedData) {
-      if (savedData.email !== undefined) {
-        form.email.value = savedData.email;
-      }
-      if (savedData.message !== undefined) {
-        form.message.value = savedData.message;
-      }
-    }
+const  KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea'); 
 
-    form.addEventListener("input", function(event) {
-      const { name, value } = event.target;
-      const trimmedValue = value.trim(); 
-  
-     
-      const formData = JSON.stringify({
-        ...savedData ?? {},
-        [name]: trimmedValue
-      });
-      localStorage.setItem("feedback-form-state", formData);
-    });
-  
- 
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const email = form.email.value.trim();
-      const message = form.message.value.trim();
+let formData = {
+  email: '',
+  message: '',
+};
 
-      if (email && message) {
-        console.log({ email, message });
-        localStorage.removeItem("feedback-form-state");
-        form.reset();
-      } else {
-        alert("Please fill in both email and message fields.");
-      }
-    });
-  });
+populateTextarea();
+
+form.addEventListener('input', onInputForm);
+form.addEventListener('submit', onSubmitForm);
+
